b9bcdf37ac7bf5f9bed7402a38bb03c2b683027c
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2024-08-30 11:35:41 +0200

    Refactor code, fix bugs, optimize performance, update README.md, update dependencies and Python version to 3.11,

--- README.md
+++ README.md
@@ -1,4 +1,133 @@
-# Gamepad Hama uRage auto configurator for Linux 
-Skrypt pythonowy do automatycznej detekcji, inicjalizacji, mapowania i kalibracji gamepada (Gamepad Hama uRage Vendetta) dla systemu Linux, przy użyciu modułu pydev i narzędzia jscal z pakietu joyutils.
+# Automatyczny konfigurator Gamepada Hama uRage Vendetta dla systemu Linux 
+Skrypt pythonowy do automatycznej detekcji, inicjalizacji, mapowania i kalibracji gamepada (Gamepad Hama uRage Vendetta) dla systemu Linux, przy użyciu modułów pyusb, pyudev i narzędzia jscal z pakietu joystick.
 
-Python script for automatic detection, initialization, mapping and calibration of the gamepad (Gamepad Hama uRage Vendetta) for Linux, using the pydev module and jscal tool from the joyutils package.
+## Cechy
+- **Automatyczne wykrywanie**: Skrypt wykorzystuje `pyudev` do monitorowania zdarzeń USB, wykrywając kiedy gamepad jest podłączony lub odłączony.
+- **Inicjalizacja**: Wysyła `kod inicjujący` w celu aktywowania gamepada.
+- **Kalibracja**: Automatycznie kalibruje gamepada za pomocą narzędzia `jscal`.
+- **Logging**: Rejestrowanie zdarzeń związanych z podłączaniem, inicjalizacją, kalibracją i odłączaniem gamepada. 
+
+## Wymagania Pythona
+- Biblioteka `pathlib`
+- Biblioteka `pyudev`
+- Biblioteka `pyusb`
+
+## Wymagania systemowe
+- Python 3.9 lub nowszy
+- `joystick` (dla `jscal`)
+
+## Dodawanie reguł Udev dla dostępu bez uprawnień roota:
+Aby umożliwić użytkownikowi inicjalizację i kalibrację gamepada bez uprawnień roota, należy skonfigurować reguły udev, które dostosowują uprawnienia do USB i urządzeń wejściowych gamepada.
+
+
+### Krok 1: Identyfikacja gamepada
+Użyj `lsusb` by zidentyfikować `idVendor` i `idProduct` twojego gamepada:
+
+```bash
+lsusb
+# Bus 003 Device 002: ID 045e:028e Microsoft Corp. Xbox360 Controller
+```
+
+IdVendor to `045e`, a idProduct to `028e`.
+
+### Krok 2: Tworzenie reguł udev
+- Utwórz nowy plik reguł udev:
+
+```bash
+sudo nano /etc/udev/rules.d/99-gamepad.rules
+```
+- Dodaj następującą zawartość, aby ustawić uprawnienia dla urządzeń INPUT/USB:
+```bash
+SUBSYSTEM=="input", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", OWNER="myuser", MODE="0600"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", OWNER="myuser", MODE="0660"
+```
+Ta konfiguracja zapewnia, że zarówno urządzenie wejściowe (np. `/dev/input/js0`), jak i samo urządzenie USB są dostępne dla wybranego użytkownika `myuser`.
+
+### Krok 3: Przeładowanie reguł Udev
+Po zapisaniu reguł, przeładuj reguły udev i uruchom je:
+
+```bash
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+```
+
+### Krok 4: Weryfikacja uprawnień
+Uruchom skrypt i spróbuj ponownie podłączyć gamepada i sprawdź uprawnienia w odpowiednich węzłach urządzenia:
+
+```bash
+ls -l /dev/input/js0
+```
+Powinieneś zobaczyć coś takiego:
+```bash
+crw-rw---- 1 myuser input 13, 0 Aug 23 15:00 /dev/input/js0
+```
+
+# Automatic configurator of the Hama uRage Vendetta Gamepad for Linux. 
+Python script for automatic detection, initialization, mapping and calibration of gamepad (Gamepad Hama uRage Vendetta) for Linux, using pyusb, pyudev modules and jscal tool from joystick package.
+
+## Features
+- **Automatic detection**: The script uses `pyudev` to monitor USB events, detecting when a gamepad is connected or disconnected.
+- **Initialization**: Sends an `initialization code` to activate the gamepad.
+- **Calibration**: Automatically calibrates the gamepad using the `jscal` tool.
+- **Logging**: Logs events related to connecting, initializing, calibrating and disconnecting the gamepad. 
+
+## Python requirements
+- `pathlib` library
+- `pyudev` library
+- `pyusb` library
+
+## Systen requirements
+- Python 3.9 or later
+- `joystick` (for `jscal`)
+
+
+## Adding Udev Rules for Non-Root Access:
+To allow user to initialize and calibrate the gamepad without root permissions, you need to set up udev rules that adjust the permissions on the gamepad's USB and input devices.
+
+
+### Step 1: Identify Your Gamepad
+Use `lsusb` to identify your gamepad's `idVendor` and `idProduct`:
+
+```bash
+lsusb
+# Bus 003 Device 002: ID 045e:028e Microsoft Corp. Xbox360 Controller
+```
+
+The idVendor is `045e` and idProduct is `028e`.
+
+### Step 2: Create Udev Rules
+- Create a new udev rules file:
+
+```bash
+sudo nano /etc/udev/rules.d/99-gamepad.rules
+```
+- Add the following content to set permissions for INPUT/USB devices:
+```bash
+SUBSYSTEM=="input", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", OWNER="myuser", MODE="0600"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="028e", OWNER="myuser", MODE="0660"
+```
+
+This configuration ensures that both the input device (e.g. `/dev/input/js0`) and the USB device itself are available to the selected `myuser` user.
+
+### Step 3: Reload Udev Rules
+
+After saving the rules, reload the udev rules and trigger them:
+
+```bash
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+```
+
+### Step 4: Verify Permissions
+
+Run script and try reconnect your gamepad and check the permissions on the relevant device nodes:
+
+```bash
+
+ls -l /dev/input/js0
+```
+You should see something like:
+```bash
+crw-rw---- 1 myuser input 13, 0 Aug 23 15:00 /dev/input/js0
+```
+This indicates that `myuser` has the necessary permissions to access and calibrate the gamepad.
--- gamepad_monitor.py
+++ gamepad_monitor.py
@@ -1,65 +1,121 @@
-#!/usr/bin/python3.9
+import shlex
 import usb
 import threading
 import logging
 import pyudev
 import time
 import subprocess
+from pathlib import Path
 
 
+class USBDetector:
+    ''' Monitor udev for detection of USB '''
 
-class USBDetector():
-    ''' Monitor udev for detection of usb '''
- 
-    def __init__(self):
+    def __init__(self, logger=None):
         ''' Initiate the object '''
-        thread = threading.Thread(target=self._work)
-        thread.daemon = False
-        thread.start()
- 
-    def _work(self):
-        ''' Runs the actual loop to detect the events '''
-        
+        self.logger = logger or self._create_default_logger()
         self.context = pyudev.Context()
         self.monitor = pyudev.Monitor.from_netlink(self.context)
         self.monitor.filter_by(subsystem='input')
-        pass
 
-        # this is module level logger, can be ignored
-        #LOGGER.info("Starting to monitor for usb")
+        # Start monitoring thread
+        self.thread = threading.Thread(target=self._work)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def _create_default_logger(self):
+        ''' Create a default logger '''
+        logger = logging.getLogger('USBDetector')
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        logger.addHandler(handler)
+        return logger
+
+    def _work(self):
+        ''' Runs the actual loop to detect the events '''
+        self.logger.info("Starting to monitor for USB devices.")
         self.monitor.start()
 
         for device in iter(self.monitor.poll, None):
-            #LOGGER.info("Got USB event: %s", device.action)
-            if device.action == 'add':
-                if device.sys_name.startswith('event'):
-                    dev = usb.core.find(idVendor=0x045e, idProduct=0x028e)
-
-                    if dev is not None:
-                        print("Init Gamepad")
-                        dev.ctrl_transfer(0xc1, 0x01, 0x0100, 0x00, 0x14)
-
-                        time.sleep(1)
-                        print("Mapping and calibrate Gamepad")
-                        subprocess.call("./xbox_gamepad_calibrate.sh")
-
-                
-                # some function to run on insertion of usb
-
-                    self.on_attached()
-
-            if device.action == 'remove':
-                
-                # some function to run on removal of usb
+            if device.action == 'add' and device.sys_name.startswith('event'):
+                self.on_attached(device)
+            elif device.action == 'remove':
                 self.on_deleted()
 
-	
-    def on_attached(self):
-        print("Gamepad attached")
+    def on_attached(self, device):
+        self.logger.info("USB device attached")
+        try:
+            dev = usb.core.find(idVendor=0x045e, idProduct=0x028e)
+            if dev:
+                event_device_path = Path(f"/sys/class/input/{device.sys_name}/device").resolve()
+                dev_input = self._get_inputx_name_for_dev(dev, event_device_path.name)
 
+                if dev_input:
+                    self.logger.info("Initializing Gamepad.")
+                    dev.ctrl_transfer(0xc1, 0x01, 0x0100, 0x00, 0x14)
+                    time.sleep(1)
+                    
+                    js_device = self._find_js_device(device.device_node)
+                    self._calibrate_device(js_device)
+
+        except usb.core.USBError as e:
+            self.logger.error(f"USB Error during initialization: {e}")
+        except subprocess.CalledProcessError as e:
+            self.logger.error(f"Calibration script failed: {e}")
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
 
     def on_deleted(self):
-        print("Gamepad dettached")
+        self.logger.info("USB device detached")
+
+    def _find_js_device(self, event_device):
+        ''' Find the correct js device associated with the given event device '''
+        self.logger.info(f"Looking for js device for {event_device}")
+        event_name = Path(event_device).name
+
+        # Look through the /dev/input/ to map event device to js device
+        for js in Path('/dev/input/').glob('js*'):
+            js_phys_path = Path(f'/sys/class/input/{js.name}/device').resolve()
+            event_phys_path = Path(f'/sys/class/input/{event_name}/device').resolve()
+
+            if js_phys_path == event_phys_path:
+                self.logger.info(f"Found matching js device: {js}")
+                return js
+
+        self.logger.error("No matching js device found.")
+        return None
+
+    def _get_inputx_name_for_dev(self, dev, device_input):
+        '''Get the name of the first input device in the given base path using glob.'''
+        base_path = Path(f'/sys/bus/usb/devices/{dev.bus}-{dev.port_number}:1.0/input/')
+        return next((input_x.name for input_x in base_path.glob('input*') if device_input == input_x.name), None)
+
+    def _calibrate_device(self, js_device):
+        '''Calibrate the given js device'''
+        if js_device:
+            command = shlex.split(f"./calibrate.sh {js_device}")
+        else:
+            self.logger.warning("Could not find js device associated with event. Trying default js0.")
+            command = shlex.split(f"./calibrate.sh")
+
+        self.logger.info("Mapping and calibrating Gamepad.")
+        result = subprocess.run(command, check=True)
+        self.logger.info(f"Calibration script finished with return code {result.returncode}.")
+
+
 
 if __name__ == '__main__':
-        USBDetector()
+    logger = logging.getLogger('USBDetector')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+
+    detector = USBDetector(logger=logger)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        detector.logger.info("Program terminated by user.")
